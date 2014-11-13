8b62629444d9e8bf8828e36403fe853c536cd1d0
ChengyuanMa <chengyuan@leandatainc.com>
2014-11-12 22:23:32 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EmailSendingWeb::Application.config.secret_token = 'ab73df0736a12bb8d66f8db0dadded18a03e59c30955eb59e7131addb14c58f3ac504affaa13ec788fadb83209ccbf2e410d2ab887ded04ebe1f2abeb1d6e7ec'
